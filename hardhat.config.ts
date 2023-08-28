ae416b1e868cca876c7f8aff338955ba72e89a93
Emanuele Spadaro <emanuelespadaro1@gmail.com>
2023-08-28 15:50:01 +0200

    feat: npx hardhat size-contracts
    also added optimizations parameters to hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@openzeppelin/hardhat-upgrades';
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: '0.8.18',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+        details: {
+          yul: true,
+        }
+      },
+      viaIR: false
+    }
+  }
 };
 
 export default config;
